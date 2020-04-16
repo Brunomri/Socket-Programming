6811f100e54bfa0ac87b5047da51a3337600734a
Bruno Richau <19270033+Brunomri@users.noreply.github.com>
2020-04-16 18:54:42 -0300

    Criar estrutura de dados para filmes

---
+++ filmes.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#define maxLen 200
+
+// TODO: Criar estrutura filme com título, sinopse, gênero, salas em exibição e identificador único
+struct filmes
+{
+	char titulo[maxLen];
+	char sinopse[maxLen];
+	char genero[maxLen];
+	char salas[maxLen];
+	int id;
+} filme;
+
+// TODO: Cadastrar novo filme e retornar identificador
+
+// TODO: Remover um filme a partir do seu identificador
+
+// TODO: Listar o título e salas de exibição de todos os filmes
+
+// TODO: Listar todos os títulos de filmes de um determinado gênero
+
+// TODO: Dado o identificador de um filme, retornar o título do filme
+
+// TODO: Dado o identificador de um filme, retornar todas as informações deste filme
+
+// TODO: Listar todas as informações de todos os filmes
+
+int main() {
+	
+	printf("Gerenciamento de filmes\n");
+
+
+}
